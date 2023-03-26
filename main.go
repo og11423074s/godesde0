7f92d9ed08bb4c87978368ae55137ff916566c6e
Osmar Gonzalez <osmarlenin.gonzalezescorche@externos-cl.cencosud.com>
2023-03-26 17:12:41 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/og11423074s/godesde0/variables"
 )
 
 func main() {
-	variables.MuestroEnteros()
+	//variables.MuestroEnteros()
+	//variables.RestoVariables()
+	estado, texto := variables.ConviertoATexto(1588)
+
+	fmt.Println(estado)
+	fmt.Println(texto)
 }
